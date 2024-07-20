2b03b5fe72c63f81fe138293cea458d5468e92d7
Ryan Tan <ryantanliner@gmail.com>
2024-07-20 11:47:56 -0700

    deleted something

--- client/src/services/login.js
+++ client/src/services/login.js
@@ -2,11 +2,6 @@ import axios from 'axios'
 const baseUrl = '/api/login'
 const userUrl = '/api/users'
 
-const getAll = () => {
-  const request = axios.get(userUrl)
-  return request.then(response => response.data)
-}
-
 const login = async credentials => {
   const response = await axios.post(baseUrl, credentials)
   return response.data
